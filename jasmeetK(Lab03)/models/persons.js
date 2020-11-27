d9db254dd8dac8996a0e1adc9a33931da8d2c536
Jasmeet Kaur <jasmeetk1811@gmail.com>
2020-11-26 22:10:21 -0500

    first commit

---
+++ jasmeetK(Lab03)/models/persons.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        'id': I,
+        'name': 'Jasmeet kaur',
+        'age': 20,
+        'Email': 'jasskaur999@gmail.com'
+    },
+    {
+        'id': II,
+        'name': 'Noah william',
+        'age': 17,
+        'Email': 'NoahW65@hotmail.com'
+    },
+    {
+        'id': III,
+        'name': 'Jenny Johal',
+        'age': 46,
+        'Email': 'Johal000@gmail.com'
+    },
+    {
+        'id': IV,
+        'name': 'Imran Khan',
+        'age': 34,
+        'Email': 'khanI45@gmail.com'
+    }
+];
